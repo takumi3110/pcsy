63915e29b2c1d4e1a645c7a3c3f2e86abe395be3
takumi3110 <takumi929s@prestigein.com>
2021-12-29 15:14:56 +0900

    add:viewset class

--- phoneplat/views.py
+++ phoneplat/views.py
@@ -1,3 +1,112 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .serializer import *
+from .models import *
+
+
 # Create your views here.
+class SiteViewSet(viewsets.ModelViewSet):
+    queryset = Site.objects.all()
+    serializer_class = SiteSerializer
+
+
+class TenantViewSet(viewsets.ModelViewSet):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class DeptViewSet(viewsets.ModelViewSet):
+    queryset = Dept.objects.all()
+    serializer_class = DeptSerializer
+
+
+class CareerViewSet(viewsets.ModelViewSet):
+    queryset = Career.objects.all()
+    serializer_class = CareerSerializer
+
+
+class LineCategoryViewSet(viewsets.ModelViewSet):
+    queryset = LineCategory.objects.all()
+    serializer_class = LineCategorySerializer
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class ContractNumberViewSet(viewsets.ModelViewSet):
+    queryset = ContractNumber.objects.all()
+    serializer_class = ContractNumberSerializer
+
+
+class ParentNumberViewSet(viewsets.ModelViewSet):
+    queryset = ParentNumber.objects.all()
+    serializer_class = ParentNumberSerializer
+
+
+class SystemViewSet(viewsets.ModelViewSet):
+    queryset = System.objects.all()
+    serializer_class = SystemSerializer
+
+
+class PhoneNumberViewSet(viewsets.ModelViewSet):
+    queryset = PhoneNumber.objects.all()
+    serializer_class = PhoneNumberSerializer
+
+
+class SurplusNumberViewSet(viewsets.ModelViewSet):
+    queryset = SurplusNumber.objects.all()
+    serializer_class = SurplusNumberSerializer
+
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class AccessLineViewSet(viewsets.ModelViewSet):
+    queryset = AccessLine.objects.all()
+    serializer_class = AccessLineSerializer
+
+
+class TrankViewSet(viewsets.ModelViewSet):
+    queryset = Trank.objects.all()
+    serializer_class = TrankSerializer
+
+
+class ScenarioViewSet(viewsets.ModelViewSet):
+    queryset = Scenario.objects.all()
+    serializer_class = ScenarioSerializer
+
+
+class TrankInfoViewSet(viewsets.ModelViewSet):
+    queryset = TrankInfo.objects.all()
+    serializer_class = TrankInfoSerializer
+
+
+class PayingServiceViewSet(viewsets.ModelViewSet):
+    queryset = PayingService.objects.all()
+    serializer_class = PayingServiceSerializer
+
+
+class IncomingNumberViewSet(viewsets.ModelViewSet):
+    queryset = IncomingNumber.objects.all()
+    serializer_class = IncomingNumberSerializer
+
+
+class PayingCodeViewSet(viewsets.ModelViewSet):
+    queryset = PayingCode.objects.all()
+    serializer_class = PayingCodeSerializer
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
