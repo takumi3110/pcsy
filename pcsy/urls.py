f3dcb4b7888db5098bf28b1cc35a292608fb327b
takumi3110 <takumi929s@prestigein.com>
2021-12-29 15:42:31 +0900

    add: include phoneplat_router

--- pcsy/urls.py
+++ pcsy/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from phoneplat.urls import router as phoneplat_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/phoneplat/', include(phoneplat_router.urls))
 ]
