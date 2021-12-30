fdefaacd44039320e3e32b7d7aefdf9626f42bb7
takumi3110 <takumi929s@prestigein.com>
2021-12-30 19:26:34 +0900

    add:filterset

--- phoneplat/filter.py
+++ phoneplat/filter.py
@@ -1,1 +1,17 @@
 # -*- cording:utf-8 -*-
+
+from django_filters import rest_framework as filters
+
+from .models import *
+
+
+class TenantFilter(filters.FilterSet):
+	class Meta:
+		model = Tenant
+		fields = ['id', 'name']
+
+
+class TeamFilter(filters.FilterSet):
+	class Meta:
+		model = Team
+		fields = ['id', 'name', 'code']
