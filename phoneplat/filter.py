a50e72fdbe3bb0d43752e1e85b13f0affd95aa4c
takumi3110 <takumi929s@prestigein.com>
2022-01-07 16:45:54 +0900

    refacter

--- phoneplat/filter.py
+++ phoneplat/filter.py
@@ -3,24 +3,6 @@ from django_filters import rest_framework as filters
 from .models import *
 
 
-class TenantFilter(filters.FilterSet):
-	class Meta:
-		model = Tenant
-		fields = ['id', 'name']
-
-
-class TeamFilter(filters.FilterSet):
-	class Meta:
-		model = Team
-		fields = ['id', 'name', 'code']
-
-
-class DeptFilter(filters.FilterSet):
-	class Meta:
-		model = Dept
-		fields = ['name', 'code']
-
-
 class ServiceFilter(filters.FilterSet):
 	class Meta:
 		model = Service
