af460e35dae82a724d867509850a531d00fbbafd
takumi3110 <takumi929s@prestigein.com>
2022-01-11 17:24:53 +0900

    add:filter

---
+++ line/filters.py
@@ -0,0 +1,27 @@
+from django_filters import rest_framework as filters
+
+from .models import *
+
+
+class CareerFilter(filters.FilterSet):
+	class Meta:
+		model = Career
+		fields = ['name']
+
+
+class CategoryFilter(filters.FilterSet):
+	class Meta:
+		model = Category
+		fields = ['name', 'career']
+
+
+class ContractNumberFilter(filters.FilterSet):
+	class Meta:
+		model = ContractNumber
+		fields = ['number', 'location', 'name', 'line_category']
+
+
+class ParentNumberFilter(filters.FilterSet):
+	class Meta:
+		model = ParentNumber
+		fields = ['number', 'career', 'line_category']
--- line/views.py
+++ line/views.py
@@ -3,23 +3,28 @@ from rest_framework import viewsets
 
 from .models import *
 from .serializers import *
+from .filters import *
 
 
 class CareerViewSet(viewsets.ModelViewSet):
     queryset = Career.objects.all()
     serializer_class = CareerSerializer
+    filter_class = CareerFilter
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_class = CategoryFilter
 
 
 class ContractNumberViewSet(viewsets.ModelViewSet):
     queryset = ContractNumber.objects.all()
     serializer_class = ContractNumberSerializer
+    filter_class = ContractNumberFilter
 
 
 class ParentNumberViewSet(viewsets.ModelViewSet):
     queryset = ParentNumber.objects.all()
     serializer_class = ParentNumberSerializer
+    filter_class = ParentNumberFilter
