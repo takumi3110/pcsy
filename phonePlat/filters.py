c33671388f8f2cace47e0424ed217bd7085b57f2
takumi3110 <takumi929s@prestigein.com>
2022-01-13 12:37:44 +0900

    add:filter class

--- phonePlat/filters.py
+++ phonePlat/filters.py
@@ -19,3 +19,51 @@ class AccessLineFilter(filters.FilterSet):
 	class Meta:
 		model = AccessLine
 		fields = ['id', 'location', 'system', 'status', 'parent_number', 'contract_number', 'updated_user']
+
+
+class PhoneNumberFilter(filters.FilterSet):
+	class Meta:
+		model = PhoneNumber
+		fields = ['id', 'category', 'number', 'system', 'paying', 'scenario', 'ticket']
+
+
+class SurplusNumberFilter(filters.FilterSet):
+	class Meta:
+		model = SurplusNumber
+		fields = ['id', 'number', 'system', 'ticket']
+
+
+class TrankFilter(filters.FilterSet):
+	class Meta:
+		model = Trank
+		fields = ['id', 'name', 'location']
+
+
+class TrankInfoFilter(filters.FilterSet):
+	class Meta:
+		model = Trank
+		fields = ['id', 'trank_primary', 'trank_secondary', 'prefix_primary', 'prefix_secondary', 'access_line']
+
+
+class PayingServiceFilter(filters.FilterSet):
+	class Meta:
+		model = PayingService
+		fields = ['id', 'name', 'abbreviation', 'career']
+
+
+class IncomingNumberFilter(filters.FilterSet):
+	class Meta:
+		model = IncomingNumber
+		fields = ['id', 'number', 'paying_service', 'holder', 'service']
+
+
+class PayingCodeFilter(filters.FilterSet):
+	class Meta:
+		model = PayingCode
+		fields = ['id', 'code', 'service', 'dept', 'target']
+
+
+class NotificationFilter(filters.FilterSet):
+	class Meta:
+		model = Notification
+		fields = ['id', 'number', 'incoming_number', 'phone_number', 'paying_code']
