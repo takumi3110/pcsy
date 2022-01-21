5444a3329f3bf3e9c6bf95e1968b171d9658d038
takumi3110 <takumi929s@prestigein.com>
2022-01-21 18:57:43 +0900

    fix:typo

--- pcsy/settings/base.py
+++ pcsy/settings/base.py
@@ -82,6 +82,8 @@ USE_I18N = True
 USE_TZ = False
 
 STATIC_URL = '/static/'
+STATIC_DIR = BASE_DIR.parent
+STATICFILES_DIRS = [os.path.join(STATIC_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
