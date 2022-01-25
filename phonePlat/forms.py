9de5bf813fb29476f7e59aec3f5cfe5d0c2dd4f3
takumi3110 <takumi929s@prestigein.com>
2022-01-25 13:13:08 +0900

    add:service_form

--- phonePlat/forms.py
+++ phonePlat/forms.py
@@ -1,1 +1,15 @@
-# -*- cording:utf-8 -*-
+from django import forms
+
+from .models import *
+
+
+class ServiceForms(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
+
+	class Meta:
+		model = Service
+		fields = ('category', 'number', 'name', 'status', 'team', 'holiday', 'remind', 'channel_count', 'start_date',
+		          'end_date', 'description')
