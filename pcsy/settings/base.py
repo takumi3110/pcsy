a32b205df8b2e4d5bf4cbc3975341d6f9ab6d223
takumi3110 <takumi929s@prestigein.com>
2022-01-25 13:10:21 +0900

    fix:login_url

--- pcsy/settings/base.py
+++ pcsy/settings/base.py
@@ -90,7 +90,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'admin/login'
+LOGIN_URL = 'admin:login'
+
+LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
 
